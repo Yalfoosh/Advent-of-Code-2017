f539b9176e17aaf688135ebbfb9f64ff249f67ff
Yalfoosh <suflajmob@gmail.com>
2017-12-06 01:48:54 +0100

    Day 5 update

---
+++ AoC_5/Core.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace AoC_5
+{
+    class Core
+    {
+        static string[] input = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\input.txt");
+        public static long[] convInput = new long[input.Length];
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int i = 0; i < input.Length; ++i)
+                    convInput[i] = long.Parse(input[i]);
+
+                Day5_1.Task();
+                Day5_2.Task();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ AoC_5/Day5_2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AoC_5
+{
+    class Day5_2
+    {
+        public static void Task()
+        {
+            int index = 0;
+            ulong counter = 0;
+
+            long[] copy = new long[Core.convInput.Length];
+
+            for (int i = 0; i < copy.Length; ++i)
+                copy[i] = Core.convInput[i];
+
+            while (true)
+            {
+                ++counter;
+
+                if (index + copy[index] >= copy.Length || index + copy[index] < 0)
+                    break;
+
+                index += copy[index] < 3 ? (int)copy[index]++ : (int)copy[index]--;
+            }
+
+            Console.WriteLine("The number of steps required to exit the loop is: " + counter + ".");
+        }
+    }
+}
